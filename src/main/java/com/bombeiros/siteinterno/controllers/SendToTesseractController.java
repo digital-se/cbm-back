73a3a1c36557b0a6acb8b8243db8948675e4b1df
Matrix2100 <vinicius.oliveira00@souunit.com.br>
2021-03-30 08:53:29 -0300

    Refatoração no controller e service para envio de imagens ao OCR, além de alteração no Mapping do controller.

--- src/main/java/com/bombeiros/siteinterno/controllers/SendToTesseractController.java
+++ src/main/java/com/bombeiros/siteinterno/controllers/SendToTesseractController.java
@@ -2,12 +2,9 @@ package com.bombeiros.siteinterno.controllers;
 
 import java.io.IOException;
 
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
+import com.bombeiros.siteinterno.services.SendToTesseractService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,51 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/testes")
+@RequestMapping("/ocrService")
 public class SendToTesseractController {
+
+    @Autowired
+    private SendToTesseractService tessService;
 
     // broken
     @CrossOrigin(origins = "http://localhost:3000")
-    @PostMapping("/getFromUrl")
-    public MultiValueMap<String,String> getFromUrl(@RequestParam(name = "file") MultipartFile file)
-            throws RestClientException, IOException {
+    @PostMapping("/sendImage")
+    public MultiValueMap<String, String> getFromUrl(@RequestParam(name = "file") MultipartFile file) throws RestClientException, IOException {
 
-        // converte arquivo para formato enviavel
-        ByteArrayResource fileConvertido = new ByteArrayResource(file.getBytes()) {
-            @Override
-            public String getFilename() {
-                return file.getOriginalFilename();
-            }
-        };
-
-        // seta o media type para o correto (opcional, não faz diferença na pratica)
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-        // define um tipo de "json" para enviar os dados
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("file", fileConvertido);
-
-        // cria entidade de resposta (opcional, não faz diferença na pratica)
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-
-        // envia um post para o serviço do ocr e recebe a resposta
-        RestTemplate restTemplate = new RestTemplate();
-        
-        // retorna a resposta do ocr (precisa mudar pro formato la de
-        // ResponseEntity.ok(resultado);
-        ResponseEntity<String> result = restTemplate.postForEntity("http://localhost:9191/ocr/extrair", requestEntity, String.class);
-
-        MultiValueMap<String,String> response = new LinkedMultiValueMap<>();
-
-        response.add("txt", result.getBody());
-
-        return response;
-
+        return tessService.send(file);
     }
 
 }
---
+++ src/main/java/com/bombeiros/siteinterno/services/SendToTesseractService.java
@@ -0,0 +1,53 @@
+package com.bombeiros.siteinterno.services;
+
+import java.io.IOException;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class SendToTesseractService {
+    
+    public MultiValueMap<String,String> send(MultipartFile file) throws IOException {
+        
+        // converte arquivo para formato enviavel
+        ByteArrayResource fileConvertido = new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+        };
+
+        // seta o media type para o correto (opcional, não faz diferença na pratica)
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        // define um tipo de "json" para enviar os dados
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", fileConvertido);
+
+        // cria entidade de resposta (opcional, não faz diferença na pratica)
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        // envia um post para o serviço do ocr e recebe a resposta
+        RestTemplate restTemplate = new RestTemplate();
+        
+        // retorna a resposta do ocr (precisa mudar pro formato la de
+        // ResponseEntity.ok(resultado);
+        ResponseEntity<String> result = restTemplate.postForEntity("http://localhost:9191/ocr/extrair", requestEntity, String.class);
+
+        MultiValueMap<String,String> response = new LinkedMultiValueMap<>();
+
+        response.add("txt", result.getBody());
+
+        return response;
+    }
+}
