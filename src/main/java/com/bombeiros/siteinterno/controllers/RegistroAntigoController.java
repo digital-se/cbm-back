1cb33593462f9403c0a70103a25d3b85b3db8bff
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-04-05 10:34:55 -0300

    Refatoração do registro antigo. Mudança no controller e service, além de alteração de path.

--- src/main/java/com/bombeiros/siteinterno/controllers/RegistroAntigoController.java
+++ src/main/java/com/bombeiros/siteinterno/controllers/RegistroAntigoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.bombeiros.siteinterno.message.ResponseFile;
+import com.bombeiros.siteinterno.message.DocumentResponseFile;
 import com.bombeiros.siteinterno.message.RegistroAntigoResponseFile;
 import com.bombeiros.siteinterno.models.RegistroAntigo;
 import com.bombeiros.siteinterno.repository.RegistroAntigoRepository;
@@ -14,13 +15,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -34,22 +35,34 @@ public class RegistroAntigoController {
     RegistroAntigoRepository registroRepository;
 
     @Autowired
-    RegistroAntigoServices registroSaveDocumento;
+    RegistroAntigoServices raServices;
 
-    @ApiOperation(value = "Retorna uma lista de Registros antigos")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "Retornou uma lista de Registro antigos"),
+    @ApiOperation(value = "Salva um novo registro antigo no DB")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Documento foi salvo com sucesso"),
             @ApiResponse(code = 404, message = "Não encontrado"),
             @ApiResponse(code = 500, message = "Foi gerada uma exceção") })
-    @GetMapping
-    public ResponseEntity<List<RegistroAntigoResponseFile>> listarRegistroAntigo() {
-        List<RegistroAntigoResponseFile> files = registroRepository.findAll().stream().map(registro -> {
-            return new RegistroAntigoResponseFile(registro.getId(), registro.getNome());
-        }).collect(Collectors.toList());
+    @PostMapping("/post")
+    @ResponseBody
+    public ResponseEntity<RegistroAntigo> salvarRegistroAntigo(@RequestPart("registro") RegistroAntigo registroAntigo,
+            @RequestPart("file") MultipartFile file) throws IOException {
 
-        return ResponseEntity.status(HttpStatus.OK).body(files);
+        raServices.salvar(registroAntigo, file);
+
+        return ResponseEntity.status(HttpStatus.OK).body(registroAntigo);
     }
 
-    @GetMapping("/documentos")
+    @ApiOperation(value = "Retorna uma lista de documentos de um registro antigo especifico")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Retornou uma lista de documentos de um registro antigo"),
+            @ApiResponse(code = 404, message = "Não encontrado"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção") })
+    @GetMapping("/get/{raid}")
+    public ResponseEntity<List<DocumentResponseFile>> listarDocumentos(@PathVariable long raid) throws IOException {
+        return ResponseEntity.status(HttpStatus.OK).body(raServices.listarDocumentos(raid));
+    }
+
+    
+
+    /* @GetMapping("/documentos")
     public ResponseEntity<List<RegistroAntigoResponseFile>> listarRegistroDocumentos() {
         List<RegistroAntigoResponseFile> files = registroRepository.findAll().stream().map(registro -> {
 
@@ -75,42 +88,18 @@ public class RegistroAntigoController {
                 }).collect(Collectors.toList());
 
         return ResponseEntity.status(HttpStatus.OK).body(files);
-    }
+    } */
 
-
-    // NEW
-
-    /* @ApiOperation(value = "Retorna uma lista de documentos de um respectivo BIR")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "Retornou uma lista de documentos de um respectivo BIR"),
+    /* @ApiOperation(value = "Retorna uma lista de Registros antigos")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Retornou uma lista de Registro antigos"),
             @ApiResponse(code = 404, message = "Não encontrado"),
             @ApiResponse(code = 500, message = "Foi gerada uma exceção") })
-    @GetMapping("/{raid}")
-    public ResponseEntity<List<RegistroAntigoResponseFile>> listarDocumentosBgo(@PathVariable long raid) {
-
-        List<RegistroAntigoResponseFile> files = registroRepository.findById(raid).stream().map(bir -> {
-            List<ResponseFile> documentos = bir.getDocumentos().stream().map(documento -> {
-                String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/todos/listar/")
-                        .path(documento.getIdDocumento().toString()).toUriString();
-
-                return new ResponseFile(documento.getIdDocumento(), documento.getName(), fileDownloadUri,
-                        documento.getType(), 0 documento.getDocumentoData().length);
-            }).collect(Collectors.toList());
-
-            return new DocumentResponseFile(bir.getIdBir(), bir.getNome(), bir.getNumBir(), documentos);
+    @GetMapping
+    public ResponseEntity<List<RegistroAntigoResponseFile>> listarRegistroAntigo() {
+        List<RegistroAntigoResponseFile> files = registroRepository.findAll().stream().map(registro -> {
+            return new RegistroAntigoResponseFile(registro.getId(), registro.getNome());
         }).collect(Collectors.toList());
 
         return ResponseEntity.status(HttpStatus.OK).body(files);
-
     } */
-
-
-    @PostMapping
-    @ResponseBody
-    public ResponseEntity<RegistroAntigo> criarRegistroAntigo(@RequestPart("registro") RegistroAntigo registroAntigo,
-            @RequestPart("file") MultipartFile file) throws IOException {
-
-        registroSaveDocumento.salvar(registroAntigo, file);
-
-        return ResponseEntity.status(HttpStatus.OK).body(registroAntigo);
-    }
 }
--- src/main/java/com/bombeiros/siteinterno/services/RegistroAntigoServices.java
+++ src/main/java/com/bombeiros/siteinterno/services/RegistroAntigoServices.java
@@ -1,19 +1,27 @@
 package com.bombeiros.siteinterno.services;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.swing.text.Document;
 import javax.transaction.Transactional;
 
+import com.bombeiros.siteinterno.message.DocumentResponseFile;
+import com.bombeiros.siteinterno.message.RegistroAntigoResponseFile;
+import com.bombeiros.siteinterno.message.ResponseFile;
 import com.bombeiros.siteinterno.models.Documento;
 import com.bombeiros.siteinterno.models.RegistroAntigo;
 import com.bombeiros.siteinterno.repository.DocumentoRepository;
 import com.bombeiros.siteinterno.repository.RegistroAntigoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Service
 public class RegistroAntigoServices {
@@ -37,6 +45,24 @@ public class RegistroAntigoServices {
         return documentoRepository.save(documento);
     }
 
+    public List<DocumentResponseFile> listarDocumentos(long raid) throws IOException {
+        List<DocumentResponseFile> files = registroAntigoRepository.findById(raid).stream().map(ra -> {
+
+            List<ResponseFile> documentos = ra.getDocumentos().stream().map(documento -> {
+
+                String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/todos/listar/")
+                        .path(documento.getIdDocumento().toString()).toUriString();
+                return new ResponseFile(documento.getIdDocumento(), documento.getName(), fileDownloadUri,
+                        documento.getType(), documento.getDocumentoData().length);
+
+            }).collect(Collectors.toList());
+            
+            //fix it
+            return new DocumentResponseFile(ra.getId(), ra.getNome(), 0, documentos);
+        }).collect(Collectors.toList());
+
+        return files;
+    }
     
      // Método de retornar uma documento pelo seu ID
      public Documento getDocumento(Long id) {
