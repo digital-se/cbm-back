23eb2f4571d310d88c12f258c36ed1f7e87a3304
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-04-12 12:01:34 -0300

    Atualização do swagger.
    Co-Authored-By: raiomarco <raiomarco@users.noreply.github.com>

--- src/main/java/com/bombeiros/siteinterno/config/SwaggerConfig.java
+++ src/main/java/com/bombeiros/siteinterno/config/SwaggerConfig.java
@@ -7,10 +7,12 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
+@EnableSwagger2WebFlux
 public class SwaggerConfig {
     
     @Bean
