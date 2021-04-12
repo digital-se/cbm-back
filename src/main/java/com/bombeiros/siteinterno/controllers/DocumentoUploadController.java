1b5814fee320df3de814b9dc9a1ed3d4824f5f7f
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-04-12 12:02:24 -0300

    DocumentoUpload comentado até segundo momento.
    Co-Authored-By: raiomarco <raiomarco@users.noreply.github.com>

--- src/main/java/com/bombeiros/siteinterno/controllers/DocumentoUploadController.java
+++ src/main/java/com/bombeiros/siteinterno/controllers/DocumentoUploadController.java
@@ -1,30 +1,30 @@
 package com.bombeiros.siteinterno.controllers;
 
-import java.util.List;
-import java.util.stream.Collectors;
+// import java.util.List;
+// import java.util.stream.Collectors;
 
-import com.bombeiros.siteinterno.message.ResponseFile;
-import com.bombeiros.siteinterno.message.ResponseMessage;
-import com.bombeiros.siteinterno.models.Documento;
-import com.bombeiros.siteinterno.services.BgaServices;
+// import com.bombeiros.siteinterno.message.ResponseFile;
+// import com.bombeiros.siteinterno.message.ResponseMessage;
+// import com.bombeiros.siteinterno.models.Documento;
+// import com.bombeiros.siteinterno.services.BgaServices;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.CacheControl;
+// import org.springframework.http.HttpHeaders;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.MediaType;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.multipart.MultipartFile;
+// import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+// import io.swagger.annotations.ApiOperation;
+// import io.swagger.annotations.ApiResponse;
+// import io.swagger.annotations.ApiResponses;
 
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,89 +32,89 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/documentos")
 public class DocumentoUploadController {
     
-    @Autowired
-    private BgaServices storageService;
+    // @Autowired
+    // private BgaServices storageService;
 
 
 
-    @ApiOperation(value = "Faz o upload de documento ")
-	@ApiResponses(value = {
-		@ApiResponse(code = 200, message = "Fez o upload de documento"),
-		@ApiResponse(code = 401, message = "Sem autenticação"),
-		@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
-		@ApiResponse(code = 404, message = "Não encontrado"),
-		@ApiResponse(code = 500, message = "Foi gerada uma exceção")
-	})
-    @PostMapping
-    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
-        String message = "";
-        if (file == null) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseMessage(message));
-        } else {
-            try {
+    // @ApiOperation(value = "Faz o upload de documento ")
+	// @ApiResponses(value = {
+	// 	@ApiResponse(code = 200, message = "Fez o upload de documento"),
+	// 	@ApiResponse(code = 401, message = "Sem autenticação"),
+	// 	@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+	// 	@ApiResponse(code = 404, message = "Não encontrado"),
+	// 	@ApiResponse(code = 500, message = "Foi gerada uma exceção")
+	// })
+    // @PostMapping
+    // public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+    //     String message = "";
+    //     if (file == null) {
+    //         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseMessage(message));
+    //     } else {
+    //         try {
             
-                storageService.salvarDocumento(file);
+    //             storageService.salvarDocumento(file);
     
-                message = "Upload de documento com sucesso: " + file.getOriginalFilename();
+    //             message = "Upload de documento com sucesso: " + file.getOriginalFilename();
     
-                return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+    //             return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
     
-            } catch (Exception e) {
-                message = "Não foi possível dar upload na documento: " + file.getOriginalFilename() + "!";
-                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-            }
-        }
+    //         } catch (Exception e) {
+    //             message = "Não foi possível dar upload na documento: " + file.getOriginalFilename() + "!";
+    //             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+    //         }
+    //     }
         
-    }
+    // }
 
     
-    @ApiOperation(value = "Retorna a lista de documentos")
-    @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "Retornou a lista de documentos"),
-        @ApiResponse(code = 401, message = "Sem autenticação"),
-        @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
-        @ApiResponse(code = 404, message = "Não encontrado"),
-        @ApiResponse(code = 500, message = "Foi gerada uma exceção")
-    })
-    @GetMapping
-    public ResponseEntity<List<ResponseFile>> getListFiles() {
-      List<ResponseFile> files = storageService.getAllDocumentos().map(documento -> {
-        String fileDownloadUri = ServletUriComponentsBuilder
-            .fromCurrentContextPath()
-            .path("/documentos/listar/")
-            .path(documento.getIdDocumento().toString())
-            .toUriString();
+    // @ApiOperation(value = "Retorna a lista de documentos")
+    // @ApiResponses(value = {
+    //     @ApiResponse(code = 200, message = "Retornou a lista de documentos"),
+    //     @ApiResponse(code = 401, message = "Sem autenticação"),
+    //     @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+    //     @ApiResponse(code = 404, message = "Não encontrado"),
+    //     @ApiResponse(code = 500, message = "Foi gerada uma exceção")
+    // })
+    // @GetMapping
+    // public ResponseEntity<List<ResponseFile>> getListFiles() {
+    //   List<ResponseFile> files = storageService.getAllDocumentos().map(documento -> {
+    //     String fileDownloadUri = ServletUriComponentsBuilder
+    //         .fromCurrentContextPath()
+    //         .path("/documentos/listar/")
+    //         .path(documento.getIdDocumento().toString())
+    //         .toUriString();
   
-        return new ResponseFile(
-            documento.getIdDocumento(),
-            documento.getName(),
-            fileDownloadUri,
-            documento.getType(),
-            documento.getDocumentoData().length);
-      }).collect(Collectors.toList());
+    //     return new ResponseFile(
+    //         documento.getIdDocumento(),
+    //         documento.getName(),
+    //         fileDownloadUri,
+    //         documento.getType(),
+    //         documento.getDocumentoData().length);
+    //   }).collect(Collectors.toList());
   
-      return ResponseEntity.status(HttpStatus.OK).body(files);
-    }
+    //   return ResponseEntity.status(HttpStatus.OK).body(files);
+    // }
 
 
-    @ApiOperation(value = "Retorna um documento pelo id")
-	@ApiResponses(value = {
-		@ApiResponse(code = 200, message = "Retornou um documento pelo id"),
-		@ApiResponse(code = 401, message = "Sem autenticação"),
-		@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
-		@ApiResponse(code = 404, message = "Não encontrado"),
-		@ApiResponse(code = 500, message = "Foi gerada uma exceção")
-	})
-    @GetMapping("/listar/{id}")
-    public ResponseEntity<byte[]> getFile(@PathVariable long id) {
+    // @ApiOperation(value = "Retorna um documento pelo id")
+	// @ApiResponses(value = {
+	// 	@ApiResponse(code = 200, message = "Retornou um documento pelo id"),
+	// 	@ApiResponse(code = 401, message = "Sem autenticação"),
+	// 	@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+	// 	@ApiResponse(code = 404, message = "Não encontrado"),
+	// 	@ApiResponse(code = 500, message = "Foi gerada uma exceção")
+	// })
+    // @GetMapping("/listar/{id}")
+    // public ResponseEntity<byte[]> getFile(@PathVariable long id) {
 
-        Documento documento = storageService.getDocumento(id);
+    //     Documento documento = storageService.getDocumentos(id);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
-        headers.setContentType(MediaType.parseMediaType(documento.getType()));
+    //     HttpHeaders headers = new HttpHeaders();
+    //     headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+    //     headers.setContentType(MediaType.parseMediaType(documento.getType()));
 
-        return new ResponseEntity<>(documento.getDocumentoData(), headers, HttpStatus.OK);
+    //     return new ResponseEntity<>(documento.getDocumentoData(), headers, HttpStatus.OK);
 
-    }
+    // }
 }
