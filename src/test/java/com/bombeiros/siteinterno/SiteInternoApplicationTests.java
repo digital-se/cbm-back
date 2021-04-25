722ef44f498da178a6f07b94595feca173ffd528
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-04-25 20:32:59 -0300

    teste antigo comentado.

--- src/test/java/com/bombeiros/siteinterno/SiteInternoApplicationTests.java
+++ src/test/java/com/bombeiros/siteinterno/SiteInternoApplicationTests.java
@@ -1,18 +1,8 @@
 package com.bombeiros.siteinterno;
 
 
-import com.bombeiros.siteinterno.models.Documento;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 
@@ -20,39 +10,40 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class SiteInternoApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+	// @Autowired
+	// private MockMvc mockMvc;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+	// @Autowired
+	// private ObjectMapper objectMapper;
 
-	@Test
-    void contextLoads() {
-    }
+	// @Test
+	// @Disabled
+    // void contextLoads() {
+    // }
 
-	// Faz o teste para verificar se o upload de imagens está realmente funcionando.
-	@Test
-	void case2() throws Exception {
+	// // Faz o teste para verificar se o upload de imagens está realmente funcionando.
+	// @Test
+	// void case2() throws Exception {
 		
-			Documento imagem = new Documento();
-			mockMvc.perform(post("/imagens")
-			.contentType("application/json")
-			.content(objectMapper.writeValueAsString(imagem)))
-			.andExpect(status().isOk());
-	}
+	// 		Documento imagem = new Documento();
+	// 		mockMvc.perform(post("/imagens")
+	// 		.contentType("application/json")
+	// 		.content(objectMapper.writeValueAsString(imagem)))
+	// 		.andExpect(status().isOk());
+	// }
 
-	// Faz o teste para verificar se a listagem de imagens está realmente funcionando.
-	@Test
-	void case3() throws Exception {
-		try {
-			Documento imagem = new Documento();
-			mockMvc.perform(get("/imagens")
-			.contentType("application/json")
-			.content(objectMapper.writeValueAsString(imagem)))
-			.andExpect(status().isOk());
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
+	// // Faz o teste para verificar se a listagem de imagens está realmente funcionando.
+	// @Test
+	// void case3() throws Exception {
+	// 	try {
+	// 		Documento imagem = new Documento();
+	// 		mockMvc.perform(get("/imagens")
+	// 		.contentType("application/json")
+	// 		.content(objectMapper.writeValueAsString(imagem)))
+	// 		.andExpect(status().isOk());
+	// 	} catch (Exception e) {
+	// 		System.out.println(e.getMessage());
+	// 	}
+	// }
 
 }
