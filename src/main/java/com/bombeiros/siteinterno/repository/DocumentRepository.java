10f1a74367bcad6d08a5996a2a8201bea27b4d7b
vinicius.gouveia <vinicius.oliveira00@souunit.com.br>
2021-05-28 09:51:12 -0300

    atualização dos repositories

--- src/main/java/com/bombeiros/siteinterno/repository/DocumentRepository.java
+++ src/main/java/com/bombeiros/siteinterno/repository/DocumentRepository.java
@@ -1,14 +1,9 @@
 package com.bombeiros.siteinterno.repository;
 
-
 import com.bombeiros.siteinterno.models.Documento;
-
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
 public interface DocumentRepository extends JpaRepository<Documento, Long> {
 
-    
 }
---
+++ src/main/java/com/bombeiros/siteinterno/repository/MilitarRepository.java
@@ -0,0 +1,9 @@
+package com.bombeiros.siteinterno.repository;
+
+import com.bombeiros.siteinterno.models.Militar;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MilitarRepository extends JpaRepository<Militar, Long> {
+
+}
