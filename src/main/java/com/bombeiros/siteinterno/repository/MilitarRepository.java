397cf22f9ce784618a78fe1c4d7f8a061ed77d98
vinicius.gouveia <vinicius.oliveira00@souunit.com.br>
2021-06-02 18:34:35 -0300

    metodo extra para o repository de militar

--- src/main/java/com/bombeiros/siteinterno/repository/MilitarRepository.java
+++ src/main/java/com/bombeiros/siteinterno/repository/MilitarRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MilitarRepository extends JpaRepository<Militar, Long> {
 
+    Militar findByNumMatricula(String numMatricula);
 }
