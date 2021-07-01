b791b15d2a42ddbebc03a5c5bd9f65ad1fe4c245
raiomarco <raiomarco@piratasdoraio.com>
2021-07-01 19:56:00 -0300

    Corrigido import para a lib correta

--- src/main/java/com/digitalse/cbm/back/entities/Documento.java
+++ src/main/java/com/digitalse/cbm/back/entities/Documento.java
@@ -1,8 +1,7 @@
 package com.digitalse.cbm.back.entities;
 
 import java.io.Serializable;
-import java.sql.Date;
-// import java.util.Date;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
