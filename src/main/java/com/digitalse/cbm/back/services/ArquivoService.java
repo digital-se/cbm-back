1d6b03e75adb62e24aeefc125c4e2357012acd82
vinicius.gouveia <vinicius.oliveira00@souunit.com.br>
2021-07-02 00:02:58 -0300

    Service for archives fixed with new entities

--- src/main/java/com/digitalse/cbm/back/services/ArquivoService.java
+++ src/main/java/com/digitalse/cbm/back/services/ArquivoService.java
@@ -1,16 +1,16 @@
 package com.digitalse.cbm.back.services;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.digitalse.cbm.back.DTO.ArquivoDTO;
-import com.digitalse.cbm.back.mappers.ArquivoMapper;
 import com.digitalse.cbm.back.entities.Arquivo;
 import com.digitalse.cbm.back.entities.Documento;
-import com.digitalse.cbm.back.repository.DocumentoRepository;
+import com.digitalse.cbm.back.mappers.ArquivoMapper;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,9 +18,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ArquivoService {
-
-    @Autowired
-    private DocumentoRepository documentoRepository;
 
     @Autowired
     private DocumentoService documentoService;
@@ -35,21 +32,24 @@ public class ArquivoService {
             ArquivoDTO tempArq = arquivosDTO.removeFirst();
             MultipartFile tempFile = files.removeFirst();
 
-            Arquivo finalArq = new Arquivo(tempArq.getArquivo_id(), doc, tempArq.getCriador(),
-                    tempArq.getDataHoraCadastro(), tempArq.getStatus(), tempArq.getNoOcr(), tempFile);
+            // new Arquivo(id, documento, nome, ocr, status, mime, tamanho, dados, criado,
+            // atualizado)
+            Arquivo finalArq = new Arquivo(tempArq.getId(), doc, tempArq.getNome(), tempArq.getOcr(),
+                    tempArq.getStatus(), tempFile.getContentType(), tempFile.getSize(),
+                    ArrayUtils.toObject(tempFile.getBytes()), tempArq.getTexto(), tempArq.getCriado(),
+                    tempArq.getAtualizado());
 
             doc.getArquivos().add(finalArq);
         }
 
         documentoService.salvar(doc);
 
-        List<Arquivo> arquivos = new ArrayList<>();
-        doc.getArquivos().forEach(arquivo -> {
-            arquivos.add(new Arquivo(arquivo.getArquivo_id(), null, arquivo.getNome(), arquivo.getTipo(),
-                    arquivo.getCriador(), arquivo.getDataHoraCadastro(), arquivo.getStatus(), arquivo.getNoOcr(),
-                    arquivo.getTamanho()));
-        });
-
+        List<Arquivo> arquivos = doc.getArquivos().stream().map(arquivo -> {
+            return new Arquivo(arquivo.getId(), /* doc */null, arquivo.getNome(), arquivo.getOcr(),
+            arquivo.getStatus(), arquivo.getMime(), arquivo.getTamanho(), /* dados */null, /* texto */null,arquivo.getCriado(),
+            arquivo.getAtualizado());
+        }).collect(Collectors.toList());
+        
         return mapper.toDTO(arquivos);
     }
 
