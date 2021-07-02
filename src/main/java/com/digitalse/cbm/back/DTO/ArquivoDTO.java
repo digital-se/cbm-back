6c5635c324997157486a77e8049ac94ac7551e54
vinicius.gouveia <vinicius.oliveira00@souunit.com.br>
2021-07-02 01:43:04 -0300

    update to archive dto with lombok nonNull

--- src/main/java/com/digitalse/cbm/back/DTO/ArquivoDTO.java
+++ src/main/java/com/digitalse/cbm/back/DTO/ArquivoDTO.java
@@ -1,5 +1,6 @@
 package com.digitalse.cbm.back.DTO;
 
+import java.util.Arrays;
 import java.util.Date;
 
 import com.digitalse.cbm.back.entities.Documento;
@@ -7,6 +8,7 @@ import com.digitalse.cbm.back.entities.Documento;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
@@ -18,13 +20,23 @@ public class ArquivoDTO {
     private Long id;
     private Documento documento;
     private String nome;
-    private Boolean ocr = false;
+    @NonNull
+    private Boolean ocr;
+    @NonNull
     private String status;
     private String mime;
     private Long tamanho;
+    @NonNull
     private Byte[] dados;
     private String texto;
     private Date criado;
     private Date atualizado;
 
+    @Override
+    public String toString() {
+        return "ArquivoDTO [atualizado=" + atualizado + ", criado=" + criado + ", dados=" + Arrays.toString(dados)
+                + ", documento=" + documento + ", id=" + id + ", mime=" + mime + ", nome=" + nome + ", ocr=" + ocr
+                + ", status=" + status + ", tamanho=" + tamanho + ", texto=" + texto + "]";
+    }
+    
 }
