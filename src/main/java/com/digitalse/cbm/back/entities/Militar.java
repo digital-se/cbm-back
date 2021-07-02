c71a6ba2486512a320c2a978a59e14c12b5bda16
vinicius.gouveia <vinicius.oliveira00@souunit.com.br>
2021-07-02 00:36:33 -0300

    militar entity with constructor from lombok

--- src/main/java/com/digitalse/cbm/back/entities/Militar.java
+++ src/main/java/com/digitalse/cbm/back/entities/Militar.java
@@ -7,20 +7,22 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "militares")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Militar {
 
     @Id
     private String matricula;
     @ManyToMany(mappedBy = "militares")
     private List<Documento> documentos;
-
-    public Militar() {
-    }
+    
 }
