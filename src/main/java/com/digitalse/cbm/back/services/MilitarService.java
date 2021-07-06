0080e299d235a623150c302f4c29a312fac094fa
vinicius.gouveia <vinicius.oliveira00@souunit.com.br>
2021-07-06 12:35:37 -0300

    buscas de militares pela api do cbm implementadas

--- src/main/java/com/digitalse/cbm/back/services/MilitarService.java
+++ src/main/java/com/digitalse/cbm/back/services/MilitarService.java
@@ -5,24 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.digitalse.cbm.back.DTO.MilitarDTO;
-import com.digitalse.cbm.back.entities.Militar;
-import com.digitalse.cbm.back.repository.MilitarRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MilitarService {
-
-    @Autowired
-    private MilitarRepository militarRepository;
-
-    // public MilitarDTO save(String matricula) throws IOException {
-    //     // return new MilitarDTO(militarRepository.save(new Militar(matricula)));
-    // }
 
     public List<MilitarDTO> getListByName(String nome) throws IOException {
         // System.out.println(URLEncoder.encode("https://sandbox-api.cbm.se.gov.br/bmrh/militares/obtermilitares/"
@@ -38,11 +28,19 @@ public class MilitarService {
         return listaMilitares;
     }
 
-    public Militar getByMatricula(String matricula) throws IOException {
-        return militarRepository.findByMatricula(matricula);
+    public List<MilitarDTO> getListByMatricula(String matricula) throws IOException {
+        // System.out.println(URLEncoder.encode("https://sandbox-api.cbm.se.gov.br/bmrh/militares/getMilitaryByMat/"
+        // + nome, "UTF-8"));
+        String url = "https://sandbox-api.cbm.se.gov.br/bmrh/militares/getMilitaryByMat/" + matricula;
+        JsonNode jsonNode = new ObjectMapper().readTree(new RestTemplate().getForObject(url, String.class));
+        System.out.println(jsonNode.toString());
+        List<MilitarDTO> listaMilitares = new ArrayList<>();
+        for (JsonNode node : jsonNode.findValues("ViewMilitar")) {
+            MilitarDTO mdto = new MilitarDTO(node.get("num_matricula").asText(), node.get("nom_completo").asText(),
+                    node.get("nom_guerra").asText(), node.get("dsc_cargo").asText());
+            listaMilitares.add(mdto);
+        }
+        return listaMilitares;
     }
 
-    public Boolean hasMilitar(String matricula) throws IOException {
-        return militarRepository.findByMatricula(matricula) != null;
-    }
 }
