581daf0cb33730cb1281ff0eb7dbfa665b8fc9b9
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-07-27 21:09:30 -0300

    removido import não utilizado

--- src/main/java/com/digitalse/cbm/back/DTO/ArquivoDTO.java
+++ src/main/java/com/digitalse/cbm/back/DTO/ArquivoDTO.java
@@ -2,8 +2,6 @@ package com.digitalse.cbm.back.DTO;
 
 import java.util.Date;
 
-import com.digitalse.cbm.back.entities.Documento;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
