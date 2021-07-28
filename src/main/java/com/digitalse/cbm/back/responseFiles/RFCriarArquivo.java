0df17c1d18d8580f8929ad06d25fcff5d9ecbd75
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-07-27 21:27:01 -0300

    adicionado constutor extra no rfcriararquivo

--- src/main/java/com/digitalse/cbm/back/responseFiles/RFCriarArquivo.java
+++ src/main/java/com/digitalse/cbm/back/responseFiles/RFCriarArquivo.java
@@ -3,6 +3,7 @@ package com.digitalse.cbm.back.responseFiles;
 import java.util.Date;
 
 import com.digitalse.cbm.back.DTO.ArquivoDTO;
+import com.digitalse.cbm.back.entities.Arquivo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,5 +30,15 @@ public class RFCriarArquivo {
         this.bucket = dto.getBucket();
         this.criado = dto.getCriado();
         this.atualizado = dto.getAtualizado();
+    }
+
+    public RFCriarArquivo(Arquivo arq) {
+        this.id = arq.getId();
+        this.documento_id = arq.getDocumento().getId();
+        this.ocr = arq.getOcr();
+        this.status = arq.getStatus();
+        this.bucket = arq.getBucket();
+        this.criado = arq.getCriado();
+        this.atualizado = arq.getAtualizado();
     }
 }
