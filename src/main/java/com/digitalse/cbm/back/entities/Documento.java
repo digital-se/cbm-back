ff4ab085a02a5c2a29bd5471e1a8f5b5935bd764
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-08-09 14:53:36 -0300

    Basic criteriaBuilder implemented

--- src/main/java/com/digitalse/cbm/back/entities/Documento.java
+++ src/main/java/com/digitalse/cbm/back/entities/Documento.java
@@ -24,9 +24,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @Entity
