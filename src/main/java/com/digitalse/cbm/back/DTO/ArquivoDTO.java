c2c78b9d437b76774594054236a824cf70d58b18
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-08-28 01:21:32 -0300

    Refatorações nos DTOs, removido IDs desnecessários

--- src/main/java/com/digitalse/cbm/back/DTO/ArquivoDTO.java
+++ src/main/java/com/digitalse/cbm/back/DTO/ArquivoDTO.java
@@ -11,18 +11,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ArquivoDTO implements IValidation {
 
-    private Long id;
-    private Long documento_id;
-    private String nome;
     private Boolean ocr = false;
     private String status = "Concluido"; // Implementar enum
     private String texto;
-    private Long bucket;
 
-    public ArquivoDTO(Long id, String nome, Boolean ocr,
-            String status) {
-        this.id = id;
-        this.nome = nome;
+    public ArquivoDTO(Boolean ocr, String status) {
         this.ocr = ocr;
         this.status = status;
     }
@@ -30,24 +23,20 @@ public class ArquivoDTO implements IValidation {
     @Override
     public boolean isValidationOk() {
         boolean nullStep = false;
-        boolean emptyStep = false;
         boolean statusStep = false;
 
-        if (this.id != null && this.documento_id != null && this.nome != null && this.ocr != null && this.status != null
-                && this.texto != null && this.bucket != null) {
+        if (this.ocr != null && this.status != null) {
             nullStep = true;
         }
 
-        if(!nome.isEmpty()) {
-            emptyStep = true;
-        }
-
-        if(status == "Concluido"){
+        if (status.contains("Concluido")) {
             statusStep = true;
         }
 
-        if(nullStep == true && emptyStep == true && statusStep == true) return true;
-        else return false;
+        if (nullStep == true && statusStep == true)
+            return true;
+        else
+            return false;
     }
 
 }
