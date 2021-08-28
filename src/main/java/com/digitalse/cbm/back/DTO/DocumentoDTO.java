6253468b9b6302e8bd8bc3210ff0a37553dc3002
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-08-28 01:21:32 -0300

    Refatorações nos DTOs, removido IDs desnecessários

--- src/main/java/com/digitalse/cbm/back/DTO/DocumentoDTO.java
+++ src/main/java/com/digitalse/cbm/back/DTO/DocumentoDTO.java
@@ -14,43 +14,42 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocumentoDTO implements IValidation {
-
-	private Long id;
 	private String nome;
 	private String numeracao;
 	private Boolean publico;
 	private String tipo;
 	private LocalDate data;
 	private String descricao;
-	/*
-	 * private OffsetDateTime criado; private OffsetDateTime atualizado;
-	 */
 	private List<Arquivo> arquivos;
 	private List<Militar> militares;
 
 	@Override
 	public boolean isValidationOk() {
 		boolean nullStep = false;
-		//boolean emptyStep = false;
+		// boolean emptyStep = false;
 		boolean tipoStep = false;
-
-		if (this.id != null && this.nome != null && this.numeracao != null && this.publico != null && this.tipo != null
+		if (this.nome != null && this.numeracao != null && this.publico != null && this.tipo != null
 				&& this.data != null && this.descricao != null && this.arquivos != null && this.militares != null) {
 			nullStep = true;
 		}
 
-		/* if (!nome.isEmpty()) {
-			emptyStep = true;
-		} */
-
-		if (tipo == "bga" || tipo == "bgo" || tipo == "bir") {
+		if (tipo.contains("bga") || tipo.contains("bgo") || tipo.contains("bir")) {
 			tipoStep = true;
 		}
 
-		if (nullStep == true && tipoStep == true)
+		if (nullStep == true && tipoStep == true) {
 			return true;
-		else
+		} else {
 			return false;
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "DocumentoDTO [arquivos=" + arquivos + ", data=" + data + ", descricao=" + descricao + ", militares="
+				+ militares + ", nome=" + nome + ", numeracao=" + numeracao + ", publico=" + publico + ", tipo=" + tipo
+				+ "]";
 	}
 
 }
