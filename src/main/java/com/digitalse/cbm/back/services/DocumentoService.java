af5a35eead7b83d22eae977870d20c671faf9576
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-08-29 23:51:53 -0300

    fix relação documento - militar

--- src/main/java/com/digitalse/cbm/back/services/DocumentoService.java
+++ src/main/java/com/digitalse/cbm/back/services/DocumentoService.java
@@ -12,7 +12,6 @@ import com.digitalse.cbm.back.DTO.DTOsDocumento.DocumentoDTO;
 import com.digitalse.cbm.back.entities.Documento;
 import com.digitalse.cbm.back.entities.Militar;
 import com.digitalse.cbm.back.mappers.DocumentoMapper;
-import com.digitalse.cbm.back.mappers.MilitarMapper;
 import com.digitalse.cbm.back.repository.DocumentoRepository;
 import com.digitalse.cbm.back.repository.MilitarRepository;
 import com.digitalse.cbm.back.repository.specifications.DocumentoSpecification;
@@ -22,7 +21,9 @@ import com.digitalse.cbm.back.responseFiles.RFsDocumento.RFEditarDocumento;
 
 import org.apache.http.HttpException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class DocumentoService {
@@ -33,6 +34,9 @@ public class DocumentoService {
     @Autowired
     private MilitarRepository militarRepository;
 
+    @Autowired
+    private MilitarService militarService;
+
     /**
      * Cria um novo documento
      * 
@@ -42,39 +46,59 @@ public class DocumentoService {
      * @throws HttpException
      */
     public RFDocumento criar(DocumentoDTO documentodto) throws IOException, HttpException {
-        if (!documentodto.isValidationOk()) throw new HttpException("validation failed");
+        if (!documentodto.isValidationOk())
+            throw new HttpException("validation failed");
+
+        RFDocumento rfdoc;
+        Documento doc = DocumentoMapper.toModel(documentodto);
+        doc.setMilitares(null);
 
         List<Militar> militares = new ArrayList<Militar>();
-    
 
-        if(documentodto.getMilitares() != null){
+        if (documentodto.getMilitares() != null) {
             documentodto.getMilitares().forEach(element -> {
-                if(!militarRepository.existsByMatricula(element.getMatricula())){
-                    militares.add(militarRepository.save(new Militar(element.getMatricula(), null)));
+                if (!militarRepository.existsByMatricula(element.getMatricula())) {
+                    try {
+                        if (militarService.getListByMatricula(element.getMatricula()) != null) {
+                            Militar mil = new Militar(element.getMatricula(), new ArrayList<Documento>());
+                            mil = militarRepository.save(mil);
+                            militares.add(mil);
+                        } else {
+                            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Envie um militar valido");
+                        }
+
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                 } else {
-                    militares.add(militarRepository.findByMatricula(element.getMatricula()));
+
+                    Militar mil = militarRepository.findByMatricula(element.getMatricula()).get();
+                    militares.add(mil);
+
                 }
             });
         }
-        Documento docModel = DocumentoMapper.toModel(documentodto);
 
-        docModel.setMilitares(militares);
+        doc.setMilitares(militares);
+        rfdoc = new RFDocumento(documentoRepository.save(doc));
 
-        RFDocumento rfdoc = new RFDocumento(documentoRepository.save(docModel));
         return rfdoc;
     }
 
     /**
      * Edita um documento e salva no banco
      * 
-     * @param id id do documento a ser editado
+     * @param id           id do documento a ser editado
      * @param documentodto dto com documento atualizado
      * @return
      * @throws IOException
      * @throws HttpException
      */
-    public RFEditarDocumento editar(long id, DocumentoDTO documentodto) throws IOException, ValidationException, HttpException {
-        if (!documentodto.isValidationOk()) throw new HttpException("erro");
+    public RFEditarDocumento editar(long id, DocumentoDTO documentodto)
+            throws IOException, ValidationException, HttpException {
+        if (!documentodto.isValidationOk())
+            throw new HttpException("erro");
 
         Documento doc = documentoRepository.findById(id).get();
 
@@ -83,22 +107,24 @@ public class DocumentoService {
         doc.setNome(documentodto.getNome());
         doc.setDescricao(documentodto.getDescricao());
         doc.setData(documentodto.getData());
+        doc.setPublico(documentodto.getPublico());
 
         List<Militar> militares = new ArrayList<Militar>();
-    
 
-        if(documentodto.getMilitares() != null){
+        if (documentodto.getMilitares() != null) {
             documentodto.getMilitares().forEach(element -> {
-                if(!militarRepository.existsByMatricula(element.getMatricula())){
-                    militares.add(militarRepository.save(new Militar(element.getMatricula(), null)));
+                if (!militarRepository.existsByMatricula(element.getMatricula())) {
+                    Militar mil = new Militar(element.getMatricula(), new ArrayList<Documento>());
+                    mil = militarRepository.save(mil);
+                    militares.add(mil);
                 } else {
-                    militares.add(militarRepository.findByMatricula(element.getMatricula()));
+                    Militar mil = militarRepository.findByMatricula(element.getMatricula()).get();
+                    militares.add(mil);
                 }
             });
         }
 
         doc.setMilitares(militares);
-        doc.setPublico(documentodto.getPublico());
         documentoRepository.save(doc);
         return new RFEditarDocumento(doc);
     }
