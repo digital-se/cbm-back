6439388cf3b3a06508938f6bb53f5b108c8e737c
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-09-02 20:27:11 -0300

    bagunça para testes complicados com o rabbit

--- src/main/java/com/digitalse/cbm/back/controllers/OcrController.java
+++ src/main/java/com/digitalse/cbm/back/controllers/OcrController.java
@@ -1,12 +1,20 @@
 package com.digitalse.cbm.back.controllers;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
+import com.digitalse.cbm.back.responseFiles.RFBucket;
+import com.digitalse.cbm.back.responseFiles.RFOcrBucket;
+import com.digitalse.cbm.back.services.OcrService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +26,26 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping
 public class OcrController {
+
+    @Autowired
+    public OcrService ocr = new OcrService();
+
+    @PostMapping("/ocr/testes")
+    public ResponseEntity<InputStreamResource> enviarImagem(@RequestParam(name = "file") MultipartFile file) throws RestClientException, IOException {
+        try {
+            //RFOcrBucket ocrbucket =  ocr.sendAmqp(file);
+            ocr.testeRabbit(file);
+            return ResponseEntity.status(HttpStatus.OK).build();
+            /* return ResponseEntity.status(HttpStatus.OK).contentLength(ocrbucket.getTamanho())
+                    .contentType(org.springframework.http.MediaType.parseMediaType(ocrbucket.getMime()))
+                    .body(new InputStreamResource(new ByteArrayInputStream(ocrbucket.getDados()))); */
+        } catch (NullPointerException e) {
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
+        }
+    }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PatchMapping("/documentos/{id}/arquivos/{id}/ocr")
