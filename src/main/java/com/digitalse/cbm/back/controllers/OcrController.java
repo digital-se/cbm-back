ccce32428e912eb54bf1eac119545232b4b45210
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-09-10 06:45:27 -0300

    endpoints para ocr http

--- src/main/java/com/digitalse/cbm/back/controllers/OcrController.java
+++ src/main/java/com/digitalse/cbm/back/controllers/OcrController.java
@@ -1,28 +1,23 @@
 package com.digitalse.cbm.back.controllers;
 
 import java.io.IOException;
+import java.util.List;
 
-import com.digitalse.cbm.back.DTO.DTOsBucket.BucketDTO;
-import com.digitalse.cbm.back.DTO.BucketOcrDTO;
-import com.digitalse.cbm.back.entities.Bucket;
-import com.digitalse.cbm.back.rabbitmq.RabbitMQConnection;
+import com.digitalse.cbm.back.DTO.DTOsBucket.BucketOcrDTO;
 import com.digitalse.cbm.back.repository.BucketRepository;
 import com.digitalse.cbm.back.responseFiles.RFBucketOcr;
-
 import com.digitalse.cbm.back.services.OcrService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -35,27 +30,12 @@ public class OcrController {
     @Autowired
     public BucketRepository bucRepo;
 
-    public ResponseEntity<String> testeEnviarOcr(@RequestParam(name = "file") Long id_bucket, Long id_arquivo)
-            throws RestClientException, IOException {
-        try {
-            Bucket bucket = bucRepo.findById(id_bucket).get();
-            ocr.sendToOcr(new BucketOcrDTO(bucket.getId(), id_arquivo, bucket.getNome(), bucket.getMime(), bucket.getTamanho(), bucket.getDados()));
-            return ResponseEntity.status(HttpStatus.OK).build();
-        } catch (NullPointerException e) {
-            System.out.println(e);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
-        }
-    }
-
-    //Paliativo para receber http do ocr
-    @CrossOrigin(origins = "http://localhost:9083")
+    // PostToBack
     @PostMapping("/ocr/receberDados")
-    public ResponseEntity<String> receber(@RequestParam(name = "file") RFBucketOcr file)
-            throws RestClientException, IOException {
+    public ResponseEntity<String> receber(@RequestBody RFBucketOcr file) throws RestClientException, IOException {
         try {
-            ocr.receiveFromOcr(file);
+            System.out.println(file.getTexto());
+            ocr.saveScannedText(file);
             return ResponseEntity.status(HttpStatus.OK).build();
         } catch (NullPointerException e) {
             System.out.println(e);
@@ -65,22 +45,11 @@ public class OcrController {
         }
     }
 
-    // RabbitMQ Implementation
-    @PostMapping("/ocr/testes")
-    public ResponseEntity<InputStreamResource> enviarImagem(@RequestParam(name = "file") MultipartFile file)
-            throws RestClientException, IOException {
+    // GetListFromBack
+    @GetMapping("/ocr/imagem/list")
+    public ResponseEntity<List<Long>> getListOfImagem() throws RestClientException, IOException {
         try {
-            // RFOcrBucket ocrbucket = ocr.sendAmqp(file);
-            ocr.sendImage(RabbitMQConnection.NOME_QUEUE_OCR, new BucketDTO(0L, file.getOriginalFilename(),
-                    file.getContentType(), file.getSize(), file.getBytes()));
-            return ResponseEntity.status(HttpStatus.OK).build();
-            /*
-             * return
-             * ResponseEntity.status(HttpStatus.OK).contentLength(ocrbucket.getTamanho())
-             * .contentType(org.springframework.http.MediaType.parseMediaType(ocrbucket.
-             * getMime())) .body(new InputStreamResource(new
-             * ByteArrayInputStream(ocrbucket.getDados())));
-             */
+            return ResponseEntity.status(HttpStatus.OK).body(ocr.getImageIds());
         } catch (NullPointerException e) {
             System.out.println(e);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -89,18 +58,47 @@ public class OcrController {
         }
     }
 
-    /* @CrossOrigin(origins = "http://localhost:3000")
-    @PatchMapping("/documentos/{id}/arquivos/{id}/ocr")
-    public ResponseEntity<MultiValueMap<String, String>> receber(@RequestParam(name = "file") MultipartFile file)
-            throws RestClientException, IOException {
+    // GetImageFromBack
+    @GetMapping("/ocr/imagem/{id_image}")
+    public ResponseEntity<BucketOcrDTO> getImagem(@PathVariable Long id_image) throws RestClientException, IOException {
         try {
-            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+            return ResponseEntity.status(HttpStatus.OK).body(ocr.sendImageToScan(id_image));
         } catch (NullPointerException e) {
             System.out.println(e);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
         }
-    } */
+    }
+
+    /*
+     * // RabbitMQ Implementation
+     * 
+     * @PostMapping("/ocr/testes2") public ResponseEntity<InputStreamResource>
+     * enviarImagem(@RequestParam(name = "file") MultipartFile file) throws
+     * RestClientException, IOException { try { // RFOcrBucket ocrbucket =
+     * ocr.sendAmqp(file); ocr.sendImage(RabbitMQConnection.NOME_QUEUE_OCR, new
+     * BucketDTO(0L, file.getOriginalFilename(), file.getContentType(),
+     * file.getSize(), file.getBytes())); return
+     * ResponseEntity.status(HttpStatus.OK).build();
+     * 
+     * } catch (NullPointerException e) { System.out.println(e); return
+     * ResponseEntity.status(HttpStatus.NOT_FOUND).build(); } catch (Exception e) {
+     * throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+     * e.getLocalizedMessage()); } }
+     */
+
+    /*
+     * @CrossOrigin(origins = "http://localhost:3000")
+     * 
+     * @PatchMapping("/documentos/{id}/arquivos/{id}/ocr") public
+     * ResponseEntity<MultiValueMap<String, String>> receber(@RequestParam(name =
+     * "file") MultipartFile file) throws RestClientException, IOException { try {
+     * return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build(); } catch
+     * (NullPointerException e) { System.out.println(e); return
+     * ResponseEntity.status(HttpStatus.NOT_FOUND).build(); } catch (Exception e) {
+     * throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+     * e.getLocalizedMessage()); } }
+     */
 
 }
